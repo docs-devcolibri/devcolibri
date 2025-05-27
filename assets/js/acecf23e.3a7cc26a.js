4b8a894748fb26aa4383b7bb146f701c4d997fc9
docs-devcolibri <devcolibri2.0@gmail.com>
2025-05-27 12:16:50 +0300

    Deploy website - based on 46b31ffc29041e937661d57687413757f58b1c5c

--- assets/js/acecf23e.3a7cc26a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdevcolibri=self.webpackChunkdevcolibri||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/devcolibri/blog","blogTitle":"Blog","authorsListPath":"/devcolibri/blog/authors"}')}}]);
